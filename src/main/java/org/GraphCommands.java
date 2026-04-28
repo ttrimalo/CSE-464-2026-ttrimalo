6ec93a8e5540123bf5139ee52d335c912fcdc4ca
Tyler Trimaloff <ttrimalo@sundevils.asu.edu>
2026-04-27 17:49:04 -0700

    Refactor 5: Create help function for choosing algorithm

--- src/main/java/org/GraphCommands.java
+++ src/main/java/org/GraphCommands.java
@@ -151,13 +151,15 @@ public class GraphCommands{
         graph.removeEdge(edge);
     }
 
-    public Path graphSearch(String src, String dst, Algorithm algo){
-        searchStrategy strategy = switch (algo) {
+    private searchStrategy createStrategy(Algorithm algo){
+        return switch (algo) {
             case BFS -> new BFSSearch(graph);
             case DFS -> new DFSSearch(graph);
             default -> new RandomWalkSearch(graph);
         };
+    }
 
-        return strategy.search(src, dst);
+    public Path graphSearch(String src, String dst, Algorithm algo){
+        return createStrategy(algo).search(src,dst);
     }
 }
